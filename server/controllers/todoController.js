f8116c12fa46208b5be2e5aeea82adc7b2c03e27
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-12 01:36:25 +0100

    libraries and controller completion

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -3,15 +3,15 @@ const ToDos = require('../models/ToDos');
 const addToDo = async (req, res, next) => {
   try {
     let newToDo = {
-      userId: req.body.userId,
+      userId: res.locals.userId,
       title: req.body.title,
       description: req.body.description,
     };
-    res.locals.userId = req.body.userId;
     let todo = await new ToDos(newToDo).save();
     return next();
   } catch (err) {
     console.log(err.message);
+    return next({ error: err });
   }
 };
 
@@ -19,7 +19,7 @@ const getToDos = async (req, res, next) => {
   try {
     const allToDos = await ToDos.find(
       { userId: res.locals.userId },
-      { userId: 0, updatedAt: 0, createdAt: 0 }
+      { userId: 0, updatedAt: 0, createdAt: 0, __v: 0 }
     );
     res.locals.allToDos = allToDos;
     return next();
@@ -31,7 +31,6 @@ const getToDos = async (req, res, next) => {
 const deleteToDo = async (req, res, next) => {
   try {
     await ToDos.deleteOne({ _id: req.params.todoId });
-    res.locals.userId = req.params.userId;
     return next();
   } catch (err) {
     console.log(err.message);
